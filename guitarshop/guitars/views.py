ab8130966f359a78495e3724637fc1edf6037e90
Vitor-Guimaraes <guimvitor@gmail.com>
2024-05-30 16:39:08 -0300

    Created index view and added path to app url file

--- guitarshop/guitars/views.py
+++ guitarshop/guitars/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from datetime import date
 
-# Create your views here.
+
+# 1. Create View
+# 2. Create URL paths, linking views.py with urls.py
+# 3. Create HTML page on templates folder
+# 4. Include App.urls file on Project.urls file
+# 5. Add app to installed apps on Project settings.py
+# 6. Add BASE_DIR to settings file to pick up global templates
+
+def index(request):
+    return render("guitars.index/html")
